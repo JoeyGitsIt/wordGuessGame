adaca1f64979d2432350ffacc920ae657077f7a1
JoeyGitsIt <josephperri3@gmail.com>
2021-09-20 17:09:58 -0400

    added some html and css

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var wordBlank = document.querySelector('.someId');
+var win = document.querySelector('.someId');
+var loss = document.querySelector('.someId');
+var timer = document.querySelector('.someId');
+var startButton = document.querySelector('.someId');
+
+var winCounter = 0;
+var lossCounter = 0;
+var numBlank = 0;
+var wordValue = "";
+var timer;
+var timerCount;
+
+var wordOptions = ["javascript", 'JSON', 'React', 'array', 'function'];
+
